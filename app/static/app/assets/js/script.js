4d5e6985b7d4d5452481fd9b593b5cab950a6800
Chidindu Emmanuel Aneke <alexjace151@gmail.com>
2024-07-23 19:21:39 +0100

    updated

--- app/static/app/assets/js/script.js
+++ app/static/app/assets/js/script.js
@@ -16,38 +16,51 @@
   /* ---------------------------------------------
          Alerts --------------------------------
   --------------------------------------------- */
-  
-  const alertBtn = document.getElementById('closeAlertButton')
-  alertBtn.onclick = () => {
-    alertBtn.parentElement.classList.add("d-none");
-  }
-  
+
+  const alertBtns = document.querySelectorAll('.closeAlertButton')
+  console.log("ALERT BTNS:", alertBtns)
+
+  alertBtns.forEach(alertBtn => {
+    if (!alertBtn) {
+      document.querySelectorAll('.alert').map(alert => alert.classList.add('d-none'))
+    }
+    alertBtn.onclick = () => {
+      alertBtn.parentElement.classList.add("d-none");
+    }
+  })
+
   /* ---------------------------------------------
          Clear input
   --------------------------------------------- */
 
-  document.getElementById('reset_min_num').onclick = function() {
-    var field = document.getElementById('min_num');
-    field.value = field.defaultValue;
-  };
+  if (document.getElementById('reset_min_num')) {
+    document.getElementById('reset_min_num').onclick = function () {
+      var field = document.getElementById('min_num');
+      field.value = field.defaultValue;
+    };
+  }
 
-  document.getElementById('reset_max_num').onclick = function() {
-    var field = document.getElementById('max_num');
-    field.value = field.defaultValue;
-  };
+  if (document.getElementById('reset_max_num')) {
+    document.getElementById('reset_max_num').onclick = function () {
+      var field = document.getElementById('max_num');
+      field.value = field.defaultValue;
+    };
+  }
 
-  document.getElementById('reset_check_particular_num').onclick = function() {
-    var field = document.getElementById('check_particular_num');
-    field.value = field.defaultValue;
-  };
+  if (document.getElementById('reset_check_particular_num')) {
+    document.getElementById('reset_check_particular_num').onclick = function () {
+      var field = document.getElementById('check_particular_num');
+      field.value = field.defaultValue;
+    };
+  }
 
   /* ---------------------------------------------
          Contact Form
   --------------------------------------------- */
 
   var form = $('.contact__form'),
-      message = $('.contact__msg'),
-      form_data;
+    message = $('.contact__msg'),
+    form_data;
 
   // Success function
   function done_func(response) {
@@ -57,8 +70,8 @@
       message.fadeOut();
     }, 4000);
     form.find('input:not([type="submit"]), textarea').val('');
-  } 
-  
+  }
+
   // fail function
   function fail_func(data) {
     message.fadeIn().removeClass('alert-success').addClass('alert-danger');
@@ -77,31 +90,31 @@
       data: form_data
     }).done(done_func).fail(fail_func);
   });
-  
-   /*
-    * ----------------------------------------------------------------------------------------
-    *  SMOTH SCROOL JS
-    * ----------------------------------------------------------------------------------------
-    */
 
-   $('a.js-scroll-trigger').on('click', function (e) {
-      var anchor = $(this);
-      $('html, body').stop().animate({
-          scrollTop: $(anchor.attr('href')).offset().top - 100
-      }, 1000);
-      e.preventDefault();
+  /*
+   * ----------------------------------------------------------------------------------------
+   *  SMOTH SCROOL JS
+   * ----------------------------------------------------------------------------------------
+   */
+
+  $('a.js-scroll-trigger').on('click', function (e) {
+    var anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $(anchor.attr('href')).offset().top - 100
+    }, 1000);
+    e.preventDefault();
   });
 
-    /* ---------------------------------------------------------- */
-      /*  Fixed header
-      /* ----------------------------------------------------------- */
-  
-      $(window).scroll(function () {
-        var window_top = $(window).scrollTop() + 1;
-        if (window_top > 50) {
-          $('.site-navigation').addClass('menu_fixed animated fadeInDown');
-        } else {
-          $('.site-navigation').removeClass('menu_fixed animated fadeInDown');
-        }
-      });
+  /* ---------------------------------------------------------- */
+  /*  Fixed header
+  /* ----------------------------------------------------------- */
+
+  $(window).scroll(function () {
+    var window_top = $(window).scrollTop() + 1;
+    if (window_top > 50) {
+      $('.site-navigation').addClass('menu_fixed animated fadeInDown');
+    } else {
+      $('.site-navigation').removeClass('menu_fixed animated fadeInDown');
+    }
+  });
 })(jQuery);
